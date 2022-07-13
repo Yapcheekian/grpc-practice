57f6a4bd7833326a681964c20cd1f5195843b18f
Gerard Yap <cheekian.yap@aiesec.net>
2022-07-13 08:10:55 +0800

    chore: update grpc version

--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
-protoc greet/greetpb/greet.proto --go-grpc_out=. # generate service stub
-protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative greet/greetpb/greet.proto # generate message stub
+# service stubs
+protoc \
+    --go-grpc_out=paths=source_relative:. \
+    greet/greetpb/greet.proto
 
 # combine service and message stubs
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative greet/greetpb/greet.proto
+protoc \
+    --go_out=paths=source_relative:. \
+    --go-grpc_out=paths=source_relative:. \
+    greet/greetpb/greet.proto
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/Yapcheekian/grpc-practice
 go 1.17
 
 require (
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220228195345-15d65a4533f7 // indirect
-	google.golang.org/grpc v1.44.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
